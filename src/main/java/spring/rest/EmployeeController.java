78ecd183bf2bb9fde803dbd841e1220b810f0185
chosi30629 <chosi306299@gmail.com>
2022-01-27 16:46:24 +0900

    Spring Rest Tutorial - ResponseEntity 적용하여 상태값 전달

--- src/main/java/spring/rest/EmployeeController.java
+++ src/main/java/spring/rest/EmployeeController.java
@@ -8,8 +8,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -93,6 +99,42 @@ public class EmployeeController {
 		return EntityModel.of(employee,
 			linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
 			linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+	}
+	
+	@PostMapping("/employees")
+	ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) {
+		EntityModel<Employee> entityModel = assembler.toModel(repository.save(newEmployee));
+		
+		return ResponseEntity
+			.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())	// status message : 201, Location: http://localhost:8080/employees/3
+			.body(entityModel);
+	}
+	
+	@PutMapping("/employees/{id}")
+	ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+		
+		Employee updatedEmployee = repository.findById(id).map(employee -> {
+			employee.setName(newEmployee.getName());
+			employee.setRole(newEmployee.getRole());
+			return repository.save(employee);
+		}).orElseGet(() -> {
+			newEmployee.setId(id);
+			return repository.save(newEmployee);
+		});
+		
+		EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+		
+		return ResponseEntity
+			.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+			.body(entityModel);
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+		
+		repository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 }
